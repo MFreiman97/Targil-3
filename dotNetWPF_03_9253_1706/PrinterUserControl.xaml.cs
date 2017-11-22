6449e7aa3a71acb472dab5ee6beaaf849d9a369a
MFreiman97 <mfreiman97@gmail.com>
2017-11-22 14:06:45 +0200

    I ARRIVED TO SECTION 5 IN THE DOC OF THE TARGIL

--- dotNetWPF_03_9253_1706/PrinterUserControl.xaml.cs
+++ dotNetWPF_03_9253_1706/PrinterUserControl.xaml.cs
@@ -20,11 +20,50 @@ namespace dotNetWPF_03_9253_1706
     /// </summary>
     public partial class PrinterUserControl : UserControl
     {
+        readonly double MAX_INK=100;
+        readonly double MIN_ADD_INK = 5;
+        readonly double MAX_PRINT_INK = 30;
+        readonly double MAX_PAGES = 400;
+        readonly double MIN_ADD_PAGES = 50;
+        readonly double MAX_PRINT_PAGES = 150;
+        private string printerName;
+
+        public string PrinterName
+        {
+            get { return printerName;}
+            set
+            {
+                PrinterNameLabel.Content = value;
+                printerName = value;
+            }
+        }
+        private double inkcount;
+
+        public double InkCount
+        {
+            get { return inkcount; }
+            set
+            {
+                inkCountProgressBar.Value = value;//making the change also in the gui
+                inkcount = value; }
+        }
+        private int pagecount;
+
+        public int PageCount
+        {
+            get { return pagecount; }
+            set
+            {
+                pageCountSlider.Value = value;//making the change also in the gui
+                pagecount = value; }
+        }
 
 
 
 
-         EventHandler<PrinterEventArgs> pageMissing;
+
+
+        EventHandler<PrinterEventArgs> pageMissing;
         EventHandler<PrinterEventArgs> inkEmpty;
         public PrinterUserControl()
         {
